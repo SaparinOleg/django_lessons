4b121f30283e448d2dc8538a32552ae33c2cba28
Saparin <SaparinOleg@gmail.com>
2023-10-14 17:15:31 +0300

    Create DB and models

--- django_blog/main/models.py
+++ django_blog/main/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Topic(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=255)
+    subscribers = models.ManyToManyField(User, through="Preference", related_name='topics')
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=10000)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
+    topics = models.ManyToManyField(Topic, related_name='topics')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    massage = models.CharField(max_length=666)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return self.massage
+
+
+class Preference(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    is_notified = models.BooleanField(default=False)
